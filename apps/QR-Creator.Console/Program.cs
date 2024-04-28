afeb1e6b0d3138d62c0981703d6261315379cc77
Fleacon <david.noskov0603@gmail.com>
2024-04-28 19:03:31 +0200

    clear program.cs

--- apps/QR-Creator.Console/Program.cs
+++ apps/QR-Creator.Console/Program.cs
@@ -1,6 +1,3 @@
 ﻿using QR_Creator.Core.QREncoding;
+using System.Text.Encodings;
 using System.Text;
-
-var AEncoder = new QREncoder(Mode.Alphanumeric, ByteEncoders.ISO);
-var NEncoder = new QREncoder(Mode.Numeric);
-var BEncoder = new QREncoder(Mode.Byte);
